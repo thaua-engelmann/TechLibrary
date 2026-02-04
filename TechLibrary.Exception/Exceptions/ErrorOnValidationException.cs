06bdca6896b1a04a93b0d55f12055a55d6f2c7cc
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-04 19:16:58 -0300

    ✨ feat: [ErrorOnValidationException] - Exception for validation errors

---
+++ TechLibrary.Exception/Exceptions/ErrorOnValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception.Exceptions;
+
+public class ErrorOnValidationException : TechLibraryException
+{
+
+    public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+    public override IReadOnlyList<string> ErrorMessages { get; }
+
+    public ErrorOnValidationException(IEnumerable<string> messages)
+    {
+        ErrorMessages = messages.ToList().AsReadOnly();
+    }
+}
