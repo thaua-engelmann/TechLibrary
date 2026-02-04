94be19404e659adf0ce6cdea96123f607b664a9e
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-04 19:20:25 -0300

    ✨ feat: [UserPostResponse] - Dto for user created response

---
+++ TechLibrary.Communication/Responses/UserPostResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace TechLibrary.Communication.Responses;
+
+public class  UserPostResponse
+{
+    public string Name { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
+}
