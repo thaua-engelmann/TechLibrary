363fa8fd51221b0451947db52de0751d1e7b88fb
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-04 19:18:57 -0300

    ✨ feat: [ErrorMessagesResponse]  - Class responsable for listing errors

---
+++ TechLibrary.Communication/Responses/ErrorMessagesResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace TechLibrary.Communication.Responses;
+
+public class ErrorMessagesResponse
+{
+    public IReadOnlyList<string> Messages { get; }
+
+    public ErrorMessagesResponse(string error)
+    {
+        Messages = [error];
+    }
+
+    public ErrorMessagesResponse(IEnumerable<string> errors)
+    {
+        Messages = errors.ToList().AsReadOnly();
+    }
+}
