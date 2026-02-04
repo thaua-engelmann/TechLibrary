df7739b898cadf062c12c9625995ab05147278b3
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-04 19:14:09 -0300

    ✨ feat: [TechLibraryException] - Create base class Exception

---
+++ TechLibrary.Exception/Exceptions/TechLibraryException.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception.Exceptions;
+public abstract class TechLibraryException : SystemException
+{
+    public abstract HttpStatusCode StatusCode { get; }
+    public abstract IReadOnlyList<string> ErrorMessages { get; }
+}
