53f6f87f00801b44ac460cce9cb5e944b68eb354
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-09 18:54:30 -0300

    ✨ feat: [ConflicException] - Create new exception for conflicts

---
+++ TechLibrary.Exception/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace TechLibrary.Exception.Exceptions;
+
+public class ConflictException(string message) : TechLibraryException
+{
+    public override HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+    public override IReadOnlyList<string> ErrorMessages { get; } = [message];
+}
