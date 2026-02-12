820ebeb023b801c60b740393cc07c916613321ae
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-12 19:03:43 -0300

    ✨ feat: [LoginPostResponse] - Dto for login response

---
+++ TechLibrary.Communication/Responses/LoginPostResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace TechLibrary.Communication.Responses;
+
+public class LoginPostResponse
+{
+    public string Username { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
