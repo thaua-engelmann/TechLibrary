18ee16e996154f07c1f7c7fdb2b8b9cf4bdf4f1e
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-12 19:03:27 -0300

    ✨ feat: [LoginPostRequest] - Dto for login request

---
+++ TechLibrary.Communication/Requests/LoginPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TechLibrary.Communication.Requests;
+
+public class LoginPostRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
