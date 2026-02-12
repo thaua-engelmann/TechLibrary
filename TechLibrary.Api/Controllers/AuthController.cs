a68df1d1fd152417dfb5253f7654e554575275aa
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-12 18:58:13 -0300

    ✨ feat: [AuthController] - Add controller responsable for login

---
+++ TechLibrary.Api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Application.UseCases.Auth.Login;
+using TechLibrary.Communication.Requests;
+
+namespace TechLibrary.Api.Controllers;
+
+public class AuthController(ILoginUseCase loginUseCase) : TechLibraryBaseController
+{
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginPostRequest request)
+    {
+        var response = await loginUseCase.Execute(request);
+        return Ok(response);
+    }
+
+}
