1de4dc915b2eb81892661ce81903851e033b2985
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-17 16:23:23 -0300

    ✨ feat: [Entity] - Add Base Entity class to be used in all other entities

---
+++ TechLibrary.Domain/Commom/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace TechLibrary.Domain.Commom;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+
+    protected void MarkUpdated()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+    protected void MarkDeleted()
+    {
+        IsDeleted = true;
+        DeletedAt = DateTime.UtcNow;
+    }
+}
